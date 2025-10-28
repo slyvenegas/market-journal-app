0a7b1af30d4335867e6b394c708b72b33cbc446a
SlyVenegas <venegasly@gmail.com>
2025-10-28 07:10:04 -0500

    widget + backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,34 @@
+import TradingViewWidget from "@/components/TradingViewWidget"
+import { MARKET_DATA_WIDGET_CONFIG } from "@/lib/constants"
+
 const Home = () => {
   return (
     <div className="flex min-h-screen home-wrapper">
-      Home
+      <section className="grip w-full gap-8 home-section">
+        <div className="md:col-span-1 xl:col-span-1">
+          <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+          </div>
+          <div className="md:col-span-1 xl:col-span-2">
+            <TradingViewWidget
+            title="Market Overview"
+            scriptUrl="https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+            config={MARKET_DATA_WIDGET_CONFIG}
+            className="custom-chart"
+            height={600}
+          />
+          </div>
+      </section>
     </div>
   )
 }
 
 export default Home
+
+
+
